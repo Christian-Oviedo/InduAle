91929fef9089660d07e3788303f6559ecd41c3ed
Christian Oviedo <44590355+Christian-Oviedo@users.noreply.github.com>
2020-09-07 03:32:31 -0300

    Add common and web projects

---
+++ InduAle.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InduAle.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
